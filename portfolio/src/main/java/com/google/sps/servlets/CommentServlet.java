b4f859908939ac3d02aec3d1627ec21bec1d9a82
Rebeka Dittrich <dittrich.rebeka@gmail.com>
2020-08-03 12:11:48 +0000

    Delete redundant comments

--- portfolio/src/main/java/com/google/sps/servlets/CommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentServlet.java
@@ -122,36 +122,27 @@ public class CommentServlet extends HttpServlet {
     return text.replaceAll("(?i)<(/?script[^>]*)>", "&lt;$1&gt;");
   }
 
-  /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
+  /* Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
   private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
     List<BlobKey> blobKeys = blobs.get(formInputElementName);
 
-    // User submitted form without selecting a file, so we can't get a URL. (dev server)
     if (blobKeys == null || blobKeys.isEmpty()) {
       return null;
     }
 
-    // Our form only contains a single file input, so get the first index.
     BlobKey blobKey = blobKeys.get(0);
 
-    // User submitted form without selecting a file, so we can't get a URL. (live server)
     BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
     if (blobInfo.getSize() == 0) {
       blobstoreService.delete(blobKey);
       return null;
     }
 
-    // We could check the validity of the file here, e.g. to make sure it's an image file
-    // https://stackoverflow.com/q/10779564/873165
-
-    // Use ImagesService to get a URL that points to the uploaded file.
     ImagesService imagesService = ImagesServiceFactory.getImagesService();
     ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
 
-    // To support running in Google Cloud Shell with AppEngine's dev server, we must use the relative
-    // path to the image, rather than the path returned by imagesService which contains a host.
     try {
       URL url = new URL(imagesService.getServingUrl(options));
       return url.getPath();
